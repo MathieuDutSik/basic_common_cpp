8279958b16830b383548d983f1ff52d279f0d291
Mathieu Dutour Sikiric <Mathieu.Dutour@gmail.com>
2018-08-03 11:33:17 +0200

    Further refinement.
      Mathieu

--- src_basic/Temp_common.h
+++ src_basic/Temp_common.h
@@ -70,6 +70,10 @@
 #include <boost/serialization/split_member.hpp>
 #include <boost/serialization/split_free.hpp>
 
+
+// Type conversion
+#incude "TypeConversion.h"
+
 // synonyms
 
 typedef unsigned long ulong;
@@ -314,14 +318,6 @@ struct is_graphbitset_class {
   static const bool value = false;
 };
 
-template<typename T1, typename T2>
-T1 UniversalTypeConversion(T2 const& a)
-{
-  T1 ret;
-  TYPE_CONVERSION(a, ret);
-  return ret;
-}
-
 
 template<typename T>
 T T_abs(T const& eVal)
@@ -331,102 +327,6 @@ T T_abs(T const& eVal)
   T fVal= - eVal;
   return fVal;
 }
-
-
-
-void NearestInteger_double_int(double const& xI, int & xO)
-{
-  //  std::cerr << "Temp_common : NearestInteger\n";
-  double xRnd_d=round(xI);
-  int xRnd_z=int(xRnd_d);
-  //  std::cerr << "xI=" << xI << "\n";
-  auto GetErr=[&](int const& u) -> double {
-    double diff = double(u) - xI;
-    return T_abs(diff);
-  };
-  double err=GetErr(xRnd_z);
-  //  std::cerr << "err=" << err << "\n";
-  while(true) {
-    bool IsOK=true;
-    for (int i=0; i<2; i++) {
-      int shift=2*i -1;
-      int xTest = xRnd_z + shift;
-      double TheErr=GetErr(xTest);
-      //      std::cerr << "i=" << i << " shift=" << shift << " xTest=" << xTest << " TheErr=" << TheErr << "\n";
-      if (TheErr < err) {
-	IsOK=false;
-	xRnd_z=xTest;
-      }
-    }
-    if (IsOK)
-      break;
-  }
-  xO=xRnd_z;
-}
-
-
-
-
-
-template<typename To>
-void NearestInteger_double_To(double const& xI, To & xO)
-{
-  //  std::cerr << "Temp_common : NearestInteger\n";
-  double xRnd_d=round(xI);
-  int xRnd_i=int(xRnd_d);
-  To xRnd_To=xRnd_i;
-  //  std::cerr << "xI=" << xI << "\n";
-  auto GetErr=[&](To const& u) -> double {
-    double u_doubl;
-    GET_DOUBLE(u, u_doubl);
-    double diff = u_doubl - xI;
-    return T_abs(diff);
-  };
-  double err=GetErr(xRnd_To);
-  //  std::cerr << "err=" << err << "\n";
-  while(true) {
-    bool IsOK=true;
-    for (int i=0; i<2; i++) {
-      int shift=2*i -1;
-      To xTest = xRnd_To + shift;
-      double TheErr=GetErr(xTest);
-      //      std::cerr << "i=" << i << " shift=" << shift << " xTest=" << xTest << " TheErr=" << TheErr << "\n";
-      if (TheErr < err) {
-	IsOK=false;
-	xRnd_To=xTest;
-      }
-    }
-    if (IsOK)
-      break;
-  }
-  xO=xRnd_To;
-}
-
-
-
-
-
-template<typename To, typename Ti>
-inline typename std::enable_if<(not is_mpreal<Ti>::value) && (not is_double_type<Ti>::value),To>::type UniversalNearestInteger(Ti const& a)
-{
-  To ret;
-  NearestInteger(a, ret);
-  //  std::cerr << "Temp_common a=" << a << " ret=" << ret << "\n";
-  return ret;
-}
-
-
-template<typename To, typename Ti>
-inline typename std::enable_if<is_double_type<Ti>::value,To>::type UniversalNearestInteger(Ti const& a)
-{
-  To ret;
-  NearestInteger_double_To<To>(a, ret);
-  //  std::cerr << "Temp_common(I) a=" << a << " ret=" << ret << "\n";
-  return ret;
-}
-
-
-
 
 
 
--- src_basic/TypeConversion.h
+++ src_basic/TypeConversion.h
@@ -10,4 +10,99 @@ T1 UniversalTypeConversion(T2 const& a)
   return ret;
 }
 
+void NearestInteger_double_int(double const& xI, int & xO)
+{
+  //  std::cerr << "Temp_common : NearestInteger\n";
+  double xRnd_d=round(xI);
+  int xRnd_z=int(xRnd_d);
+  //  std::cerr << "xI=" << xI << "\n";
+  auto GetErr=[&](int const& u) -> double {
+    double diff = double(u) - xI;
+    return T_abs(diff);
+  };
+  double err=GetErr(xRnd_z);
+  //  std::cerr << "err=" << err << "\n";
+  while(true) {
+    bool IsOK=true;
+    for (int i=0; i<2; i++) {
+      int shift=2*i -1;
+      int xTest = xRnd_z + shift;
+      double TheErr=GetErr(xTest);
+      //      std::cerr << "i=" << i << " shift=" << shift << " xTest=" << xTest << " TheErr=" << TheErr << "\n";
+      if (TheErr < err) {
+	IsOK=false;
+	xRnd_z=xTest;
+      }
+    }
+    if (IsOK)
+      break;
+  }
+  xO=xRnd_z;
+}
+
+
+
+
+
+template<typename To>
+void NearestInteger_double_To(double const& xI, To & xO)
+{
+  //  std::cerr << "Temp_common : NearestInteger\n";
+  double xRnd_d=round(xI);
+  int xRnd_i=int(xRnd_d);
+  To xRnd_To=xRnd_i;
+  //  std::cerr << "xI=" << xI << "\n";
+  auto GetErr=[&](To const& u) -> double {
+    double u_doubl;
+    GET_DOUBLE(u, u_doubl);
+    double diff = u_doubl - xI;
+    return T_abs(diff);
+  };
+  double err=GetErr(xRnd_To);
+  //  std::cerr << "err=" << err << "\n";
+  while(true) {
+    bool IsOK=true;
+    for (int i=0; i<2; i++) {
+      int shift=2*i -1;
+      To xTest = xRnd_To + shift;
+      double TheErr=GetErr(xTest);
+      //      std::cerr << "i=" << i << " shift=" << shift << " xTest=" << xTest << " TheErr=" << TheErr << "\n";
+      if (TheErr < err) {
+	IsOK=false;
+	xRnd_To=xTest;
+      }
+    }
+    if (IsOK)
+      break;
+  }
+  xO=xRnd_To;
+}
+
+
+
+
+
+template<typename To, typename Ti>
+inline typename std::enable_if<(not is_mpreal<Ti>::value) && (not is_double_type<Ti>::value),To>::type UniversalNearestInteger(Ti const& a)
+{
+  To ret;
+  NearestInteger(a, ret);
+  //  std::cerr << "Temp_common a=" << a << " ret=" << ret << "\n";
+  return ret;
+}
+
+
+template<typename To, typename Ti>
+inline typename std::enable_if<is_double_type<Ti>::value,To>::type UniversalNearestInteger(Ti const& a)
+{
+  To ret;
+  NearestInteger_double_To<To>(a, ret);
+  //  std::cerr << "Temp_common(I) a=" << a << " ret=" << ret << "\n";
+  return ret;
+}
+
+
+
+
+
 #endif
