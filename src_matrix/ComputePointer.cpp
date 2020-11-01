5ef9ca66dd98b3fa7a7a082916f0e7fc416e7312
Mathieu Dutour Sikiric <mathieu.dutour@gmail.com>
2020-11-01 17:59:08 +0100

    Add new file showing the functionality.

---
+++ src_matrix/ComputePointer.cpp
@@ -0,0 +1,23 @@
+#include "MAT_MatrixInt.h"
+#include "NumberTheory.h"
+int main(int argc, char *argv[])
+{
+  using T=int;
+  int n=5;
+  int m=7;
+  MyMatrix<T> eM(n,m);
+  for (int i=0; i<n; i++)
+    for (int j=0; j<m; j++)
+      eM(i,j) = 11 * i + j;
+  T* ptr = eM.data();
+  for (int i=0; i<n; i++)
+    for (int j=0; j<m; j++) {
+      T val1 = eM(i,j);
+      int pos = j*n + i;
+      T val2 = ptr[pos];
+      if (val1 != val2) {
+        std::cerr << "Inconsistency at i=" << i << " j=" << j << "\n";
+      }
+    }
+
+}
