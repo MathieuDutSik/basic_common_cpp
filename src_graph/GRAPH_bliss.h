4e8822fc6b7a07366bee613ebddf95942d229d03
Mathieu Dutour Sikiric <mathieu.dutour@gmail.com>
2020-11-14 12:30:17 +0100

    Insertion of some code from another section for clarity.

--- src_graph/GRAPH_bliss.h
+++ src_graph/GRAPH_bliss.h
@@ -9,7 +9,37 @@
 #include "utils.hh"
 
 
-
+bliss::Graph* ReadGraphFromFile(FILE *f, unsigned int &nof_vertices)
+{
+  unsigned int nof_edges;
+  int ret;
+  bliss::Graph *g =0;
+  ret=fscanf(f, "%u %u\n", &nof_vertices, &nof_edges);
+  if (ret != 1) {
+    std::cerr << "fscanf error while reading graph 1\n";
+    throw TerminalException{1};
+  }
+  g = new bliss::Graph(nof_vertices);
+  for (int i=0; i<int(nof_vertices); i++) {
+    unsigned int color;
+    ret=fscanf(f, "%u\n", &color);
+    if (ret != 1) {
+      std::cerr << "fscanf error while reading graph 2\n";
+      throw TerminalException{1};
+    }
+    g->change_color(i, color);
+  }
+  for (int iEdge=0; iEdge<int(nof_edges); iEdge++) {
+    int a, b;
+    ret=fscanf(f, "%u %u\n", &a, &b);
+    if (ret != 1) {
+      std::cerr << "fscanf error while reading graph 3\n";
+      throw TerminalException{1};
+    }
+    g->add_edge(a-1, b-1);
+  }
+  return g;
+}
 
 static inline void report_aut_void(void* param, const unsigned int n, const unsigned int* aut)
 {
