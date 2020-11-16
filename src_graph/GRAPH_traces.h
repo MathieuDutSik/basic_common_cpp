7fbb92ee72f7715355db6123ee648e9421c13b5f
Mathieu Dutour Sikiric <Mathieu.Dutour@gmail.com>
2020-11-16 19:05:59 +0100

    Cleaning of the print statements.

--- src_graph/GRAPH_traces.h
+++ src_graph/GRAPH_traces.h
@@ -119,16 +119,13 @@ std::vector<std::vector<unsigned int>> TRACES_GetListGenerators(Tgr const& eGR)
       }
     }
     /* Calling Traces */
-    std::cerr << "Before Traces\n";
     Traces(&sg1,lab1,ptn,orbits,&options,&stats,NULL);
-    std::cerr << "After Traces\n";
     /* Extracting the list of generators */
     std::vector<std::vector<unsigned int>> ListGen;
     //
     permnode* pn = gens;
     do
       {
-        std::cerr << "Inserting generator\n";
         std::vector<unsigned int> V(n);
         for (int i=0; i<n; i++)
           V[i] = pn->p[i];
