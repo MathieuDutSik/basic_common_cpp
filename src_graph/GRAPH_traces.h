eecdc0ec0c16a75326ea8debf8340133c475df9d
Mathieu Dutour Sikiric <mathieu.dutour@gmail.com>
2020-11-21 20:11:15 +0100

    More corrections.

--- src_graph/GRAPH_traces.h
+++ src_graph/GRAPH_traces.h
@@ -41,6 +41,7 @@ std::vector<unsigned int> TRACES_GetCanonicalOrdering(Tgr const& eGR)
       if (eGR.GetHasVertexColor())
         eColor = eGR.GetColor(i);
       ptn[i] = eColor;
+      std::cerr << "i=" << i << " eColor=" << eColor << "\n";
     }
     int pos = 0;
     for (int i=0; i<n; i++) {
@@ -85,9 +86,14 @@ std::vector<std::vector<unsigned int>> TRACES_GetListGenerators(Tgr const& eGR)
     freeschreier(NULL,&gens);
     options.getcanon = FALSE;
 
+    /* Reading key graph variables */
+    int n = eGR.GetNbVert();
+    int nbAdjacent = eGR.GetNbAdjacent();
+    bool HasVertexColor = eGR.GetHasVertexColor();
+    std::cerr << "n=" << n << " nbAdjacent=" << nbAdjacent << " HasVerteColor=" << HasVertexColor << "\n";
+
     /* Declare and initialize sparse graph structures */
     SG_DECL(sg1);
-    int n = eGR.GetNbVert();
     int m = SETWORDSNEEDED(n);
     nauty_check(WORDSIZE,m,n,NAUTYVERSIONID);
 
@@ -95,18 +101,64 @@ std::vector<std::vector<unsigned int>> TRACES_GetListGenerators(Tgr const& eGR)
     DYNALLOC1(int,ptn,ptn_sz,n,"malloc");
     DYNALLOC1(int,orbits,orbits_sz,n,"malloc");
     DYNALLOC1(int,map,map_sz,n,"malloc");
+    /*
+      "lab" and "ptn" contain the information of the initial partition.
+      It is a complex construction that goes deep into the internals of nauty/traces.
+      ---Initially ptn contains only two possible values: NAUTY_INFINITY and 0
+      ---
+      Initia
+     */
+    if (HasVertexColor) {
+      options.defaultptn = FALSE;
+      int numcells=0;
+      for (int i=0; i<n; i++) {
+        int eVal = 1 + eGR.GetColor(i);
+        if (eVal > numcells)
+          numcells = eVal;
+      }
+      std::vector<int> ListPartSize(numcells,0);
+      for (int i=0; i<n; i++)
+        ListPartSize[eGR.GetColor(i)]++;
+      std::vector<int> ListShift(numcells,0);
+      for (int icell=1; icell<numcells; icell++)
+        ListShift[icell] = ListShift[icell-1] + ListPartSize[icell-1];
+      std::vector<int> PartReord(n, 0);
+      for (int i=0; i<n; i++) {
+        int icell = eGR.GetColor(i);
+        PartReord[ListShift[icell]] = i;
+        ListShift[icell]++;
+      }
+      for (int icell=0; icell<numcells; icell++)
+        ListShift[icell] -= ListPartSize[icell];
+      std::cerr << "ListShift/ListPartSize =";
+      for (int i=0; i<n; i++) ptn[i] = NAUTY_INFINITY;
+      int i=0;
+      int j=-1;
+      for (int icell=0; icell<numcells; icell++) {
+        for (int idx=0; idx<ListPartSize[icell]; idx++) {
+          lab1[++j] = PartReord[ListShift[icell] + idx];
+        }
+        if (j >= i) {
+          ptn[j] = 0;
+        }
+        if (icell < numcells-1) {
+          i = j + 1;
+        }
+      }
+      std::cerr << "lab1 =";
+      for (int i=0; i<n; i++)
+        std::cerr << " " << lab1[i];
+      std::cerr << "\n";
+      std::cerr << "ptn =";
+      for (int i=0; i<n; i++)
+        std::cerr << " " << ptn[i];
+      std::cerr << "\n";
+    }
 
     /* Now make the first graph */
-    int nbAdjacent = eGR.GetNbAdjacent();
     SG_ALLOC(sg1,n,nbAdjacent,"malloc");
     sg1.nv = n;              /* Number of vertices */
     sg1.nde = nbAdjacent;           /* Number of directed edges */
-    for (int i=0; i<n; i++) {
-      int eColor = 0;
-      if (eGR.GetHasVertexColor())
-        eColor = eGR.GetColor(i);
-      ptn[i] = eColor;
-    }
     int pos = 0;
     for (int i=0; i<n; i++) {
       std::vector<int> LAdj = eGR.Adjacency(i);
@@ -119,7 +171,9 @@ std::vector<std::vector<unsigned int>> TRACES_GetListGenerators(Tgr const& eGR)
       }
     }
     /* Calling Traces */
+    std::cerr << "Before Traces\n";
     Traces(&sg1,lab1,ptn,orbits,&options,&stats,NULL);
+    std::cerr << "After Traces\n";
     /* Extracting the list of generators */
     std::vector<std::vector<unsigned int>> ListGen;
     //
