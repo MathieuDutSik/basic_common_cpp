4fb08238026caa3d0b8376aba2225da4923a2e8b
Mathieu Dutour Sikiric <mathieu.dutour@gmail.com>
2020-11-23 10:25:29 +0100

    Another attempt.

--- src_number/NumberTheory.h
+++ src_number/NumberTheory.h
@@ -214,16 +214,14 @@ namespace std {
 // to_string functionality
 
 namespace std {
-  template <>
-  to_string<mpz_class>(const mpz_class& e_val)
+  to_string(const mpz_class& e_val)
   {
     std::stringstream s;
     s << e_val;
     std::string converted(s.str());
     return converted;
   };
-  template <>
-  to_string<mpq_class>(const mpq_class& e_val)
+  to_string(const mpq_class& e_val)
   {
     std::stringstream s;
     s << e_val;
