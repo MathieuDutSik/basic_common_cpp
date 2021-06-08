bafc5d14e6e7d3ec371732a52fc23e3ffc63550e
Mathieu Dutour Sikiric <mathieu.dutour@gmail.com>
2021-06-08 08:00:09 +0200

    Some change to references.

--- src_graph/GRAPH_traces.h
+++ src_graph/GRAPH_traces.h
@@ -62,19 +62,19 @@ public:
 
 
 template<typename Tidx>
-std::vector<Tidx> TRACES_GetCanonicalOrdering_Arr(DataTraces* DT)
+std::vector<Tidx> TRACES_GetCanonicalOrdering_Arr(DataTraces& DT)
 {
     static DEFAULTOPTIONS_TRACES(options);
     TracesStats stats;
-    int n = DT->n;
+    int n = DT.n;
 
     options.getcanon = TRUE;
     options.defaultptn = FALSE;
 
-    Traces(&DT->sg1, DT->lab1, DT->ptn, DT->orbits, &options, &stats, &DT->cg1);
+    Traces(&DT.sg1, DT.lab1, DT.ptn, DT.orbits, &options, &stats, &DT.cg1);
     std::vector<Tidx> V(n);
     for (int i=0; i<n; i++)
-      V[DT->lab1[i]] = i;
+      V[DT.lab1[i]] = i;
     return V;
 }
 
@@ -223,14 +223,14 @@ std::vector<Tidx> TRACES_GetCanonicalOrdering_Arr_Test(Tgr const& eGR)
 {
   DataTraces DT = GetDataTraces_from_G(eGR);
   std::cerr << "After TRACES_GetCanonicalOrdering_Arr_Test\n";
-  return TRACES_GetCanonicalOrdering_Arr<Tidx>(&DT);
+  return TRACES_GetCanonicalOrdering_Arr<Tidx>(DT);
 }
 
 
 
 
 template<typename Tidx>
-std::vector<std::vector<Tidx>> TRACES_GetListGenerators_Arr(DataTraces* DT, int n_last)
+std::vector<std::vector<Tidx>> TRACES_GetListGenerators_Arr(DataTraces& DT, int n_last)
 {
     static DEFAULTOPTIONS_TRACES(options);
     TracesStats stats;
@@ -241,14 +241,14 @@ std::vector<std::vector<Tidx>> TRACES_GetListGenerators_Arr(DataTraces* DT, int 
     options.getcanon = FALSE;
 
     /*
-    int n = DT->n;
+    int n = DT.n;
     int m = SETWORDSNEEDED(n);
     nauty_check(WORDSIZE,m,n,NAUTYVERSIONID);
     */
 
     options.defaultptn = FALSE;
 
-    Traces(&DT->sg1, DT->lab1, DT->ptn, DT->orbits, &options, &stats, NULL);
+    Traces(&DT.sg1, DT.lab1, DT.ptn, DT.orbits, &options, &stats, NULL);
 
     std::vector<std::vector<Tidx>> ListGen;
     if (gens) {
@@ -375,12 +375,12 @@ std::vector<std::vector<Tidx>> TRACES_GetListGenerators_Arr_Test(Tgr const& eGR,
 {
   DataTraces DT = GetDataTraces_from_G(eGR);
   std::cerr << "After TRACES_GetListGenerators_Arr_Test\n";
-  return TRACES_GetListGenerators_Arr<Tidx>(&DT, n_last);
+  return TRACES_GetListGenerators_Arr<Tidx>(DT, n_last);
 }
 
 
 template<typename Tidx>
-std::pair<std::vector<Tidx>, std::vector<std::vector<Tidx>>> TRACES_GetCanonicalOrdering_ListGenerators_Arr(DataTraces* DT, int n_last)
+std::pair<std::vector<Tidx>, std::vector<std::vector<Tidx>>> TRACES_GetCanonicalOrdering_ListGenerators_Arr(DataTraces& DT, int n_last)
 {
     static DEFAULTOPTIONS_TRACES(options);
     TracesStats stats;
@@ -389,14 +389,14 @@ std::pair<std::vector<Tidx>, std::vector<std::vector<Tidx>>> TRACES_GetCanonical
     gens = NULL;
     freeschreier(NULL,&gens);
 
-    int n = DT->n;
+    int n = DT.n;
     options.getcanon = TRUE;
     options.defaultptn = FALSE;
 
-    Traces(&DT->sg1, DT->lab1, DT->ptn, DT->orbits, &options, &stats, &DT->cg1);
+    Traces(&DT.sg1, DT.lab1, DT.ptn, DT.orbits, &options, &stats, &DT.cg1);
     std::vector<Tidx> V(n);
     for (int i=0; i<n; i++)
-      V[DT->lab1[i]] = i;
+      V[DT.lab1[i]] = i;
     std::vector<std::vector<Tidx>> ListGen;
     if (gens) {
       permnode* pn = gens;
