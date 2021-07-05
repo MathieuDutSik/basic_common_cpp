beafef692b35ae785a5f511be1c676aa952d6e69
Mathieu Dutour Sikiric <mathieu.dutour@gmail.com>
2021-07-05 08:33:51 +0200

    Addition of robin_hood hash function.

--- src_basic/hash_functions.h
+++ src_basic/hash_functions.h
@@ -329,6 +329,75 @@ uint32_t murmur3_32(const uint8_t* key, size_t len, uint32_t seed)
 }
 
 
+
+template<typename T>
+inline T unaligned_load(void const* ptr) noexcept {
+    // using memcpy so we don't get into unaligned load problems.
+    // compiler should optimize this very well anyways.
+    T t;
+    std::memcpy(&t, ptr, sizeof(T));
+    return t;
+}
+
+
+// Code adapted from robin_hood hash function
+inline size_t robin_hood_hash_bytes(void const* ptr, size_t len, const uint64_t& seed) noexcept {
+    static constexpr uint64_t m = UINT64_C(0xc6a4a7935bd1e995);
+    //    static constexpr uint64_t seed = UINT64_C(0xe17a1465);
+    static constexpr unsigned int r = 47;
+
+    auto const* const data64 = static_cast<uint64_t const*>(ptr);
+    uint64_t h = seed ^ (len * m);
+
+    size_t const n_blocks = len / 8;
+    for (size_t i = 0; i < n_blocks; ++i) {
+        auto k = unaligned_load<uint64_t>(data64 + i);
+
+        k *= m;
+        k ^= k >> r;
+        k *= m;
+
+        h ^= k;
+        h *= m;
+    }
+    auto const* const data8 = reinterpret_cast<uint8_t const*>(data64 + n_blocks);
+    switch (len & 7U) {
+    case 7:
+        h ^= static_cast<uint64_t>(data8[6]) << 48U;
+        [[fallthrough]];
+    case 6:
+        h ^= static_cast<uint64_t>(data8[5]) << 40U;
+        [[fallthrough]];
+    case 5:
+        h ^= static_cast<uint64_t>(data8[4]) << 32U;
+        [[fallthrough]];
+    case 4:
+        h ^= static_cast<uint64_t>(data8[3]) << 24U;
+        [[fallthrough]];
+    case 3:
+        h ^= static_cast<uint64_t>(data8[2]) << 16U;
+        [[fallthrough]];
+    case 2:
+        h ^= static_cast<uint64_t>(data8[1]) << 8U;
+        [[fallthrough]];
+    case 1:
+        h ^= static_cast<uint64_t>(data8[0]);
+        h *= m;
+        [[fallthrough]];
+    default:
+        break;
+    }
+
+    h ^= h >> r;
+
+    // not doing the final step here, because this will be done by keyToIdx anyways
+    // h *= m;
+    // h ^= h >> r;
+    return static_cast<size_t>(h);
+}
+
+
+
 namespace std {
   template <typename T>
   struct hash<std::vector<T>>
