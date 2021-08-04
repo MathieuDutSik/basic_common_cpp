513abbb3d197072bd5794ef3aa34eccd1076a7a5
Mathieu Dutour Sikiric <mathieu.dutour@gmail.com>
2021-08-04 18:48:40 +0200

    Separation.

--- src_comb/COMB_Combinatorics_elem.h
+++ src_comb/COMB_Combinatorics_elem.h
@@ -2,7 +2,6 @@
 #define INCLUDE_COMBINATORICS_ELEM
 
 #include "Temp_common.h"
-#include "MAT_Matrix.h"
 
 std::vector<int> BinomialStdvect_First(int const& k)
 {
@@ -28,47 +27,6 @@ bool BinomialStdvect_Increment(int const&n, int const&k, std::vector<int> & Tvec
   if (Tvect[k-1] == n)
     return false;
   return true;
-}
-
-
-
-MyMatrix<int> BuildSet(int const& n, int const& Nval)
-{
-  int TotalNb=MyPow<int>(Nval, n);
-  std::vector<int> eVect(n, 0);
-  std::function<int(void)> fUpdate=[&]() -> int {
-    for (int iVar=0; iVar<n; iVar++)
-      if (eVect[iVar] < Nval-1) {
-	eVect[iVar]++;
-	for (int jVar=0; jVar<iVar; jVar++)
-	  eVect[jVar]=0;
-	return 0;
-      }
-    return -1;
-  };
-  MyMatrix<int> RetMatrix(TotalNb, n);
-  int idx=0;
-  while(true) {
-    for (int i=0; i<n; i++)
-      RetMatrix(idx,i)=eVect[i];
-    idx++;
-    int test=fUpdate();
-    if (test == -1)
-      break;
-  }
-  return RetMatrix;
-}
-
-
-int PositionBuildSet(int const& n, int const& Nval, MyVector<int> const& V)
-{
-  int pos=0;
-  int eProd=1;
-  for (int i=0; i<n; i++) {
-    pos += eProd * V(i);
-    eProd *= Nval;
-  }
-  return pos;
 }
 
 
