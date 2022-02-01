150b7cd45ac6124fe98eb838b8e80eac5c9c302f
Mathieu Dutour Sikiric <mathieu.dutour@gmail.com>
2022-02-01 10:46:02 +0100

    One more test program.

---
+++ src_matrix/RemoveFractionMatrix.cpp
@@ -0,0 +1,29 @@
+#include "NumberTheory.h"
+#include "MAT_MatrixInt.h"
+int main(int argc, char *argv[])
+{
+  using T=mpq_class;
+  try {
+    if (argc != 2) {
+      fprintf(stderr, "Number of argument is = %d\n", argc);
+      fprintf(stderr, "This program is used as\n");
+      fprintf(stderr, "RemoveFractionMatrix [inputMat]\n");
+      return -1;
+    }
+    // reading the matrix
+    std::ifstream INmat(argv[1]);
+    MyMatrix<T> M=ReadMatrix<T>(INmat);
+    //
+    std::cerr << "M=\n";
+    WriteMatrix(std::cerr, M);
+    //
+    FractionMatrix<T> eRec = RemoveFractionMatrixPlusCoeff(M);
+    //
+    std::cerr << "Mult=" << eRec.TheMult << "\n";
+    std::cerr << "TheMat=\n";
+    WriteMatrix(std::cerr, eRec.TheMat);
+  }
+  catch (TerminalException const& e) {
+    exit(e.eVal);
+  }
+}
