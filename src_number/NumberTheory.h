6a2197ca957d3962ca1dc7f7ed10279dfb667bb1
Mathieu Dutour Sikiric <mathieu.dutour@gmail.com>
2022-04-04 18:59:07 +0200

    Repositionng.

--- src_number/NumberTheory.h
+++ src_number/NumberTheory.h
@@ -2,9 +2,9 @@
 #define SRC_NUMBER_NUMBERTHEORY_H_
 
 #include "TemplateTraits.h"
+#include "ResidueQuotient.h"
 #include "NumberTheoryGeneric.h"
 #include "NumberTheoryGmp.h"
-#include "ResidueQuotient.h"
 #include "TypeConversion.h"
 
 #endif // SRC_NUMBER_NUMBERTHEORY_H_
