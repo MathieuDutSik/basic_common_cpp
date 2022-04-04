820d0fb7473fd54411cb9af29f6c4fba66d97b86
Mathieu Dutour Sikiric <mathieu.dutour@gmail.com>
2022-04-04 18:36:50 +0200

    Add some code.

---
+++ src_number/NumberTheoryRational.h
@@ -0,0 +1,10 @@
+#ifndef INCLUDE_NUMBER_THEORY_RATIONAL_H_
+#define INCLUDE_NUMBER_THEORY_RATIONAL_H_
+
+#include "TemplateTraits.h"
+#include "NumberTheoryGeneric.h"
+#include "ResidueQuotient.h"
+#include "TypeConversion.h"
+#include "rational.h"
+
+#endif
