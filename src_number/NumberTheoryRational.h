6ff56731f2222f8dc76e99d7ab6f950057acade4
Mathieu Dutour Sikiric <mathieu.dutour@gmail.com>
2022-04-04 18:39:19 +0200

    Repositioning.

--- src_number/NumberTheoryRational.h
+++ src_number/NumberTheoryRational.h
@@ -2,8 +2,8 @@
 #define INCLUDE_NUMBER_THEORY_RATIONAL_H_
 
 #include "TemplateTraits.h"
-#include "NumberTheoryGeneric.h"
 #include "ResidueQuotient.h"
+#include "NumberTheoryGeneric.h"
 #include "TypeConversion.h"
 #include "rational.h"
 
