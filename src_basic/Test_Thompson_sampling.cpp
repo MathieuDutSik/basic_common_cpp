eb47911865cea4fa64f806538df1d87182046401
Mathieu Dutour Sikiric <mathieu.dutour@gmail.com>
2022-07-13 15:07:37 +0200

    Add one more test.

---
+++ src_basic/Test_Thompson_sampling.cpp
@@ -0,0 +1,11 @@
+// Copyright (C) 2022 Mathieu Dutour Sikiric <mathieu.dutour@gmail.com>
+#include "Heuristic_fct.h"
+
+void test(const std::vector<std::string> &LStr1,
+          const std::vector<std::string> &LStr2) {
+
+}
+
+int main() {
+
+}
