4d6c382b8e8c045b52bea1ea95fda1eabcfb077e
Mathieu Dutour Sikiric <mathieu.dutour@gmail.com>
2022-12-30 15:21:18 +0100

    Add threshold code (first version, not tested)

---
+++ src_number/NumberTheoryThreshold.h
@@ -0,0 +1,327 @@
+// Copyright (C) 2022 Mathieu Dutour Sikiric <mathieu.dutour@gmail.com>
+#ifndef SRC_NUMBER_REALFIELD_H_
+#define SRC_NUMBER_REALFIELD_H_
+
+#include "NumberTheory.h"
+#include "Temp_common.h"
+#include "MAT_Matrix.h"
+
+// For general real field.
+// We need to use more sophisticated and slower algorithms than quadratic fields.
+// (A) linear algebra is needed.
+// (B) analysis is needed for deciding signs.
+
+
+
+std::map<int,double> list_threshold;
+
+
+void insert_helper_real_algebraic_field(int i_field, double const& thr)
+{
+  list_threshold.emplace(i_field, thr);
+}
+
+template <int i_field> class ThesholdField {
+public:
+  using T = double;
+private:
+  T a;
+public:
+  T& get_val() {
+    return a;
+  }
+  const T& get_const_val() const {
+    return a;
+  }
+
+  // Note: We are putting "int" as argument here because we want to do the comparison with the
+  // stuff like x > 0 or x = 1.
+  // For the type "rational<T>" we had to forbid that because this lead to erroneous conversion
+  // of say int64_t to int with catastrophic loss of precision.
+  // But for the QuadField<T> the loss of precision does not occur because T is typically mpq_class.
+  // or some other type that does not convert to integers easily. And at the same time the natural
+  // conversion of int to int64_t allows the comparison x > 0 and equality set x = 1 to work despite
+  // the lack of a operator=(int const& u)
+
+  // Constructor
+  ThresholdField() {
+    a = 0;
+  }
+  ThresholdField(int const &u) {
+    a = UniversalScalarConversion<T,int>(u);
+  }
+  Thresholdield(T const &u) {
+    a = u;
+  }
+  ThresholdField(RealField<i_field> const &x) : a(x.a) {}
+  //  QuadField<T,d>& operator=(QuadField<T,d> const&); // assignment operator
+  //  QuadField<T,d>& operator=(T const&); // assignment operator from T
+  //  QuadField<T,d>& operator=(int const&); // assignment operator from T
+  // assignment operator from int
+  RealField<i_field> operator=(int const &u) {
+    a = UniversalScalarConversion<T,int>(u);
+    return *this;
+  }
+  // assignment operator
+  RealField<i_field> operator=(RealField<i_field> const &x) {
+    a = x.a;
+    return *this;
+  }
+  //
+  // Arithmetic operators below:
+  void operator+=(RealField<i_field> const &x) {
+    a += x.a;
+  }
+  void operator-=(RealField<i_field> const &x) {
+    a -= x.a;
+  }
+  void operator/=(RealField<i_field> const &x) {
+    a = a / x.a;
+  }
+  friend RealField<i_field> operator+(RealField<i_field> const &x,
+                                      RealField<i_field> const &y) {
+    T val = x.a + y.a;
+    return RealField<i_field>(val);
+  }
+  friend RealField<i_field> operator-(RealField<i_field> const &x,
+                                      RealField<i_field> const &y) {
+    T val = x.a - y.a;
+    return RealField<i_field>(val);
+  }
+  friend RealField<i_field> operator-(RealField<i_field> const &x, int const &y) {
+    T val = x.a - UniversalScalarConversion<T,int>(y);
+    return RealField<i_field>(val);
+  }
+  friend RealField<i_field> operator-(RealField<i_field> const &x) {
+    T val = a - x.a;
+    return RealField<i_field>(cal);
+  }
+  friend RealField<i_field> operator/(int const &x, RealField<i_field> const &y) {
+    T val = UniversalScalarConversion<T,int>(x) / y.a;
+    return RealField<i_field>(val);
+  }
+  friend RealField<i_field> operator/(RealField<i_field> const &x,
+                                      RealField<i_field> const &y) {
+    T val = x.a / y.a;
+    return RealField<i_field>(val);
+  }
+  double get_d() const {
+    return UniversalScalarConversion<double,T>(a)
+  }
+  void operator*=(RealField<i_field> const &x) {
+    a += x.a;
+  }
+  friend RealField<i_field> operator*(RealField<i_field> const &x,
+                                      RealField<i_field> const &y) {
+    T val = x.a / y.a;
+    return RealField<i_field>(val);
+  }
+  friend RealField<i_field> operator*(int const &x, RealField<i_field> const &y) {
+    T val = UniversalScalarConversion<T,int>(x) * y.a;
+    return RealField<i_field>(val);
+  }
+  friend std::ostream &operator<<(std::ostream &os, RealField<i_field> const &v) {
+    os << v.a;
+    return os;
+  }
+  friend std::istream &operator>>(std::istream &is, RealField<i_field> &v) {
+    T val;
+    is >> val;
+    v = RealField<i_field>(V);
+    return is;
+  }
+  friend bool operator==(RealField<i_field> const &x, RealField<i_field> const &y) {
+    double const& thr = list_threshold.at(i_field);
+    if (T_abs(x.a - y.a) < thr)
+      return true;
+    return false;
+  }
+  friend bool operator!=(RealField<i_field> const &x, RealField<i_field> const &y) {
+    double const& thr = list_threshold.at(i_field);
+    if (T_abs(x.a - y.a) < thr)
+      return false;
+    return true;
+  }
+  friend bool operator!=(RealField<i_field> const &x, int const &y) {
+    double const& thr = list_threshold.at(i_field);
+    if (T_abs(x.a - UniversalScalarConversion<T,int>(y)) < thr)
+      return false;
+    return true;
+  }
+  friend bool operator>=(RealField<i_field> const &x, RealField<i_field> const &y) {
+    double const& thr = list_threshold.at(i_field);
+    if (x.a >= y.a - thr)
+      return true;
+    return false;
+  }
+  friend bool operator>=(RealField<i_field> const &x, int const &y) {
+    double const& thr = list_threshold.at(i_field);
+    if (x.a >= UniversalScalarConversion<T,int>(y) - thr)
+      return true;
+    return false;
+  }
+  friend bool operator<=(RealField<i_field> const &x, RealField<i_field> const &y) {
+    double const& thr = list_threshold.at(i_field);
+    if (x.a <= y.a + thr)
+      return true;
+    return false;
+  }
+  friend bool operator<=(RealField<i_field> const &x, int const &y) {
+    double const& thr = list_threshold.at(i_field);
+    if (x.a <= UniversalScalarConversion<T,int>(y) + thr)
+      return true;
+    return false;
+  }
+  friend bool operator>(RealField<i_field> const &x, RealField<i_field> const &y) {
+    double const& thr = list_threshold.at(i_field);
+    if (x.a > y.a + thr)
+      return true;
+    return false;
+  }
+  friend bool operator>(RealField<i_field> const &x, int const &y) {
+    double const& thr = list_threshold.at(i_field);
+    if (x.a > UniversalScalarConversion<T,int>(y) + thr)
+      return true;
+    return false;
+  }
+  friend bool operator<(RealField<i_field> const &x, RealField<i_field> const &y) {
+    double const& thr = list_threshold.at(i_field);
+    if (x.a < y.a - thr)
+      return true;
+    return false;
+  }
+  friend bool operator<(RealField<i_field> const &x, int const &y) {
+    double const& thr = list_threshold.at(i_field);
+    if (x.a < UniversalScalarConversion<T,int>(y) - thr)
+      return true;
+    return false;
+  }
+};
+
+// For this construction we cannot hope to handle rings and fields nicely
+
+template<int i_field>
+struct overlying_field<RealField<i_field>> { typedef RealField<i_field> field_type; };
+
+// Note that the underlying ring is not unique, there are many possibiliies actually
+// but we can represent only one in our scheme.
+template<int i_field>
+struct underlying_ring<RealField<i_field>> { typedef RealField<i_field> ring_type; };
+
+template <int i_field>
+inline void TYPE_CONVERSION(stc<RealField<i_field>> const &eQ, double &eD) {
+  eD = eQ.val.get_d();
+}
+
+template <int i_field>
+inline void TYPE_CONVERSION(stc<RealField<i_field>> const &eQ, RealField<i_field> &eD) {
+  eD = eQ.val;
+}
+
+template <int i_field> struct is_totally_ordered<RealField<i_field>> {
+  static const bool value = true;
+};
+
+template <int i_field> struct is_ring_field<RealField<i_field>> {
+  static const bool value = true;
+};
+
+template <int i_field> struct is_exact_arithmetic<RealField<i_field>> {
+  static const bool value = true;
+};
+
+// Hashing function
+
+template <int i_field>
+struct is_implementation_of_Z<RealField<i_field>> {
+  static const bool value = false;
+};
+
+template <int i_field>
+struct is_implementation_of_Q<RealField<i_field>> {
+  static const bool value = false;
+};
+
+// Hashing function
+
+namespace std {
+  template <int i_field> struct hash<RealField<i_field>> {
+    std::size_t operator()(const RealField<i_field> &x) const {
+      auto combine_hash = [](size_t &seed, size_t new_hash) -> void {
+      	seed ^= new_hash + 0x9e3779b9 + (seed << 6) + (seed >> 2);
+      };
+      size_t seed = 0x9e2479b9;
+      std::vector<mpq_class> V = x.get_const_seq();
+      for (auto & val : V) {
+        size_t e_hash = std::hash<mpq_class>()(val);
+        combine_hash(seed, e_hash);
+      }
+      return seed;
+    }
+  };
+}
+
+
+// Local typing info
+
+template<typename T> struct is_real_algebraic_field {};
+
+template<int i_field> struct is_real_algebraic_field<RealField<i_field>> { static const bool value = true; };
+
+// Some functionality
+
+template<int i_field>
+bool IsInteger(RealField<i_field> const& x) {
+  std::vector<mpq_class> V = x.get_const_seq();
+  size_t len = V.size();
+  for (size_t u=1; u<len; u++)
+    if (V[u] != 0)
+      return false;
+  return IsInteger(V[0]);
+}
+
+// The conversion tools (int)
+
+template<typename T2, int i_field>
+inline typename std::enable_if<not is_real_algebraic_field<T2>::value, void>::type
+TYPE_CONVERSION(stc<RealField<i_field>> const &x1, T2 &x2) {
+  std::vector<mpq_class> const& V = x1.val.get_const_seq();
+  size_t len = V.size();
+  for (size_t u=1; u<len; u++) {
+    if (V[u] != 0) {
+      std::string str = "Conversion error for quadratic field";
+      throw ConversionException{str};
+    }
+  }
+  stc<mpq_class> a1 { V[0] };
+  TYPE_CONVERSION(a1, x2);
+}
+
+// Serialization stuff
+
+namespace boost::serialization {
+
+template <class Archive, int i_field>
+inline void serialize(Archive &ar, RealField<i_field> &val,
+                      [[maybe_unused]] const unsigned int version) {
+  std::vector<mpq_class> & V = val.get_seq();
+  for (auto & val : V)
+    ar &make_nvp("realfield_seq", val);
+}
+
+}
+
+// Turning into something rational
+
+template<typename Tring, int i_field>
+void ScalingInteger_Kernel(stc<RealField<i_field>> const& x, Tring& x_res) {
+  std::vector<Tring> V;
+  for (auto & val : x.val.get_const_seq())
+    V.push_back(GetDenominator_z(val));
+  x_res = LCMlist(V);
+}
+
+
+// clang-format off
+#endif  // SRC_NUMBER_REALFIELD_H_
+// clang-format on
