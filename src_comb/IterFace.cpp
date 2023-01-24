26e8e46b6845cb730501d3057f8a9173295fb02d
Mathieu Dutour Sikiric <mathieu.dutour@gmail.com>
2023-01-24 14:32:41 +0100

    Add the test program for iterating.

---
+++ src_comb/IterFace.cpp
@@ -0,0 +1,28 @@
+// Copyright (C) 2022 Mathieu Dutour Sikiric <mathieu.dutour@gmail.com>
+#include "Boost_bitset_kernel.h"
+#include "MAT_Matrix.h"
+
+int main() {
+  size_t n = 20;
+  Face f(n);
+  for (size_t i = 0; i < n; i++)
+    f[i] = random() % 2;
+
+  std::cerr << "f (V1) =";
+  for (int i=0; i<n; i++)
+    std::cerr << " " << f[i];
+  std::cerr << "\n";
+
+  std::cerr << "f (V2) =";
+  for (int i=0; i<n; i++)
+    if (f[i] == 1)
+      std::cerr << " " << i;
+  std::cerr << "\n";
+
+  //  std::cerr << "f (V3) =";
+  //  for (auto& eVal : f) {
+  //    std::cerr << " " << eVal;
+  //  }
+  //  std::cerr << "\n";
+
+}
