60d41945f837698a67c2b911def8af4aa208cec2
Mathieu Dutour Sikiric <mathieu.dutour@gmail.com>
2023-02-05 09:27:15 +0100

    Some cleanup.

--- src_linbox/nullspacebasis_rat.cpp
+++ src_linbox/nullspacebasis_rat.cpp
@@ -1,34 +1,3 @@
-// Copyright (C) 2022 Mathieu Dutour Sikiric <mathieu.dutour@gmail.com>
-/*
- * examples/nullspacebasis.C
- *
- * Copyright (C) 2014 J-G. Dumas
- * ========LICENCE========
- * This file is part of the library LinBox.
- *
- * LinBox is free software: you can redistribute it and/or modify
- * it under the terms of the  GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
- * ========LICENCE========
- */
-
-/**\file examples/nullspacebasis.C
- * @example  examples/nullspacebasis.C
-  \brief NullSpace of sparse matrix over GFq.
-  \brief nullspace is allocated m \times n.
-  \ingroup examples
-  */
-
 #include "linbox/algorithms/gauss.h"
 #include "linbox/matrix/dense-matrix.h"
 #include <givaro/gfq.h>
@@ -37,7 +6,6 @@
 using namespace LinBox;
 
 int main(int argc, char **argv) {
-
   if (argc != 2) {
     std::cerr << "Usage to get a null space basis over Q:  "
                  "<matrix-file-in-SMS-format>"
@@ -70,11 +38,3 @@ int main(int argc, char **argv) {
 
   return 0;
 }
-
-// Local Variables:
-// mode: C++
-// tab-width: 4
-// indent-tabs-mode: nil
-// c-basic-offset: 4
-// End:
-// vim:sts=4:sw=4:ts=4:et:sr:cino=>s,f0,{0,g0,(0,\:0,t0,+0,=s
