cd96e7b0810c33297cb4601d31e552715afacbbe
Mathieu Dutour Sikiric <mathieu.dutour@gmail.com>
2023-03-27 13:58:49 +0200

    Introduction of multiple arithmetic for NullspaceComputation.

--- src_matrix/NullspaceComputation.cpp
+++ src_matrix/NullspaceComputation.cpp
@@ -1,29 +1,92 @@
 // Copyright (C) 2022 Mathieu Dutour Sikiric <mathieu.dutour@gmail.com>
-#include "MAT_Matrix.h"
+// clang-format off
 #include "NumberTheory.h"
+#include "NumberTheoryRealField.h"
+#include "QuadField.h"
+#include "MAT_Matrix.h"
+// clang-format on
+
+
+template<typename T>
+void full_process_type(std::string const& input, std::string const& output) {
+  MyMatrix<T> TheMat = ReadMatrixFile<T>(input);
+  std::cerr << "TheMat=\n";
+  WriteMatrix(std::cerr, TheMat);
+  std::cerr << "\n";
+  //
+  MyMatrix<T> TheKer = NullspaceMat(TheMat);
+  std::cerr << "TheKer=\n";
+  WriteMatrix(std::cerr, TheKer);
+  std::cerr << "\n";
+  //
+  std::ofstream os(output);
+  os << "return ";
+  WriteMatrixGAP(os, TheKer);
+  os << ";\n";
+}
+
+
+void process(std::string const& arith, std::string const& input, std::string const& output) {
+  if (arith == "rational") {
+    using T = mpq_class;
+    return full_process_type<T>(input, output);
+  }
+  if (arith == "Qsqrt5") {
+    using Trat = mpq_class;
+    using T = QuadField<Trat, 5>;
+    return full_process_type<T>(input, output);
+  }
+  if (arith == "Qsqrt2") {
+    using Trat = mpq_class;
+    using T = QuadField<Trat, 2>;
+    return full_process_type<T>(input, output);
+  }
+  std::optional<std::string> opt_realalgebraic =
+    get_postfix(arith, "RealAlgebraic=");
+  if (opt_realalgebraic) {
+    std::string const &FileAlgebraicField = *opt_realalgebraic;
+    if (!IsExistingFile(FileAlgebraicField)) {
+      std::cerr << "FileAlgebraicField=" << FileAlgebraicField
+                << " is missing\n";
+      throw TerminalException{1};
+    }
+    using T_rat = mpq_class;
+    HelperClassRealField<T_rat> hcrf(FileAlgebraicField);
+    int const idx_real_algebraic_field = 1;
+    insert_helper_real_algebraic_field(idx_real_algebraic_field, hcrf);
+    using T = RealField<idx_real_algebraic_field>;
+    return full_process_type<T>(input, output);
+  }
+  std::cerr << "Failed to find a matching entry for arith\n";
+  throw TerminalException{1};
+}
+
+
+
 int main(int argc, char *argv[]) {
-  using T = mpq_class;
+  SingletonTime time1;
   try {
-    if (argc != 3) {
+    if (argc != 4) {
       fprintf(stderr, "Number of argument is = %d\n", argc);
       fprintf(stderr, "This program is used as\n");
-      fprintf(stderr, "NullspaceComputation [inputMat] [output]\n");
+      fprintf(stderr, "NullspaceComputation [arith] [inputMat] [output]\n");
+      fprintf(stderr, "    where\n");
+      fprintf(stderr, "arith: The arithmetic type\n");
+      fprintf(stderr, "inputMat: The input matrix\n");
+      fprintf(stderr, "output: The output matrix\n");
       return -1;
     }
-    // reading the matrix
-    std::ifstream INmat(argv[1]);
-    MyMatrix<T> TheMat = ReadMatrix<T>(INmat);
-    // computing the kernel
-    Eigen::FullPivLU<MyMatrix<T>> lu(TheMat);
-    MyMatrix<T> TheKer = lu.kernel();
-
-    //    MyMatrix<T> TheKer=NullspaceMat(TheMat);
     //
-    std::ofstream os(argv[2]);
-    os << "return ";
-    WriteMatrixGAP(os, TheKer);
-    os << ";\n";
+    std::string arith = argv[1];
+    std::string input = argv[2];
+    std::string output = argv[3];
+    //
+    process(arith, input, output);
+    //
+    std::cerr << "Normal termination of the program\n";
   } catch (TerminalException const &e) {
+    std::cerr << "Something wrong happened in the computation\n";
     exit(e.eVal);
   }
+  runtime(time1);
 }
--- src_matrix/NullspaceComputationExperiment.cpp
+++ src_matrix/NullspaceComputationExperiment.cpp
@@ -1,28 +1,23 @@
 // Copyright (C) 2022 Mathieu Dutour Sikiric <mathieu.dutour@gmail.com>
 #include "MAT_Matrix.h"
 #include "NumberTheory.h"
-#include "QuadField.h"
-
 int main(int argc, char *argv[]) {
-  using Trat = mpq_class;
-  using T = QuadField<Trat, 5>;
+  using T = mpq_class;
   try {
     if (argc != 3) {
       fprintf(stderr, "Number of argument is = %d\n", argc);
       fprintf(stderr, "This program is used as\n");
-      fprintf(stderr, "NullspaceComputationQ5 [inputMat] [output]\n");
+      fprintf(stderr, "NullspaceComputation [inputMat] [output]\n");
       return -1;
     }
     // reading the matrix
     std::ifstream INmat(argv[1]);
     MyMatrix<T> TheMat = ReadMatrix<T>(INmat);
-    std::cerr << "TheMat=\n";
-    WriteMatrix(std::cerr, TheMat);
-    std::cerr << "\n";
-    MyMatrix<T> TheKer = NullspaceMat(TheMat);
-    std::cerr << "TheKer=\n";
-    WriteMatrix(std::cerr, TheKer);
-    std::cerr << "\n";
+    // computing the kernel
+    Eigen::FullPivLU<MyMatrix<T>> lu(TheMat);
+    MyMatrix<T> TheKer = lu.kernel();
+
+    //    MyMatrix<T> TheKer=NullspaceMat(TheMat);
     //
     std::ofstream os(argv[2]);
     os << "return ";
