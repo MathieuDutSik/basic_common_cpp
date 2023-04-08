104b9ebc28787ecb618144d3776f4853a31f0d05
Mathieu Dutour Sikiric <mathieu.dutour@gmail.com>
2023-04-08 13:48:36 +0200

    Add some more function.

--- src_basic/Timings.h
+++ src_basic/Timings.h
@@ -104,6 +104,11 @@ template<typename T>
 struct TimeEval {
   std::chrono::time_point<std::chrono::system_clock> time;
   TimeEval() { time = std::chrono::system_clock::now(); }
+  int64_t const_eval_int64() const {
+    std::chrono::time_point<std::chrono::system_clock> timeNew =
+        std::chrono::system_clock::now();
+    return std::chrono::duration_cast<T>(timeNew - time).count();
+  }
   std::string eval() {
     std::chrono::time_point<std::chrono::system_clock> timeNew =
         std::chrono::system_clock::now();
