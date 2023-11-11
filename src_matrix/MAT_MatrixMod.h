9c699c5bacb27e846509d2503b90d9b3d10ecd51
Mathieu Dutour Sikiric <mathieu.dutour@gmail.com>
2023-11-11 10:57:47 +0100

    Upgrade of isotropic stuff.

--- src_matrix/MAT_MatrixMod.h
+++ src_matrix/MAT_MatrixMod.h
@@ -142,14 +142,68 @@ template <typename T> T mod_inv(T const &a, T const &P) {
   return t;
 }
 
+
+template<typename T>
+bool Kernel_FindIsotropicVectorQuadResidue(MyMatrix<T> const &M, MyVector<T> & V, T const &TheMod) {
+  int n = M.rows();
+  T cst = 0;
+  for (int i = 0; i < n - 1; i++) {
+    for (int j = 0; j < n - 1; j++) {
+      cst += M(i, j) * V(i) * V(j);
+    }
+  }
+  T lin = 0;
+  for (int i = 0; i < n - 1; i++) {
+    lin += M(i, n - 1) * V(i);
+    }
+  T C = M(n - 1, n - 1);
+  // The equation to be solved becomes
+  // 0 = cst + 2 lin x_n + C x_n^2
+  // If C = 0 then solution is obvious.
+  // Otherwise, divide by C and get
+  // 0 = cst + 2 lin x_n + x_n^2
+  // 0 = cst + (x_n + lin)^2 - lin^2
+  // lin^2 - cst = (x_n + lin)^2
+  T Cinv = mod_inv(C, TheMod);
+  lin *= Cinv;
+  cst *= Cinv;
+  T a = lin * lin - cst;
+  std::optional<T> opt = find_quadratic_residue(a, TheMod);
+  if (opt) {
+    T const &y = *opt;
+    T xn = y - lin;
+    V(n - 1) = xn;
+#ifdef DEBUG_MOD_OPERATION
+    T sum = 0;
+    for (int i = 0; i < n; i++)
+      for (int j = 0; j < n; j++)
+        sum += M(i, j) * V(i) * V(j);
+    T res = ResInt(sum, TheMod);
+    if (res != 0) {
+      std::cerr << "We failed to find an isotropic vector\n";
+      throw TerminalExcpetion{1};
+    }
+#endif
+    return true;
+  }
+  return false;
+}
+
 /*
   Find isotropic vector
   We follow here the paper "Quadratic equations in dimensions 4, 5 and more"
   and in particular Lemma 1.
 */
-MyVector<T> FindIsotropicVector(MyMatrix<T> const &M, T const &TheMod) {
+template<typename T>
+MyVector<T> FindIsotropicVectorRandom(MyMatrix<T> const &M, T const &TheMod) {
   int n = M.rows();
   MyVector<T> V(n);
+  if (M(n-1,n-1) == 0) {
+    for (int i=0; i<n-1; i++)
+      V(i) = 0;
+    V(n-1) = 1;
+    return true;
+  }
   while (true) {
     // We set up the first n-1 coordinates at random
     // Then we solve the equation for finding the last one.
@@ -158,48 +212,60 @@ MyVector<T> FindIsotropicVector(MyMatrix<T> const &M, T const &TheMod) {
       T val_T(val);
       V(i) = val_T;
     }
-    T cst = 0;
-    for (int i = 0; i < n - 1; i++) {
-      for (int j = 0; j < n - 1; j++) {
-        cst += M(i, j) * V(i) * V(j);
-      }
-    }
-    T lin = 0;
-    for (int i = 0; i < n - 1; i++) {
-      lin += M(i, n - 1) * V(i);
-    }
-    T C = M(n - 1, n - 1);
-    // The equation to be solved becomes
-    // 0 = cst + 2 lin x_n + C x_n^2
-    // If C = 0 then solution is obvious.
-    // Otherwise, divide by C and get
-    // 0 = cst + 2 lin x_n + x_n^2
-    // 0 = cst + (x_n + lin)^2 - lin^2
-    // lin^2 - cst = (x_n + lin)^2
-    T Cinv = mod_inv(C, TheMod);
-    lin *= Cinv;
-    cst *= Cinv;
-    T a = lin * lin - cst;
-    std::optional<T> opt = find_quadratic_residue(a, TheMod);
-    if (opt) {
-      T const &y = *opt;
-      T xn = y - lin;
-      V(n - 1) = xn;
-#ifdef DEBUG_MOD_OPERATION
-      T sum = 0;
-      for (int i = 0; i < n; i++)
-        for (int j = 0; j < n; j++)
-          sum += M(i, j) * V(i) * V(j);
-      T res = ResInt(sum, TheMod);
-      if (res != 0) {
-        std::cerr << "We failed to find an isotropic vector\n";
-        throw TerminalExcpetion{1};
-      }
-#endif
+    bool test = Kernel_FindIsotropicVectorQuadResidue(M, V, TheMod);
+    if (test) {
       return V;
     }
   }
 }
+
+/*
+  About the solution for two-dimensional.
+ */
+template<typename T>
+std::optional<MyVector<T>> FindIsotropicVectorTwoDim(MyMatrix<T> const &M, T const &TheMod) {
+  MyVector<T> V(2);
+  if (M(1,1) == 0) {
+    V(0) = 0;
+    V(1) = 1;
+    return V;
+  }
+  V(0) = 1;
+  bool test = Kernel_FindIsotropicVectorQuadResidue(M, V, TheMod);
+  if (test) {
+    return V;
+  }
+  return {};
+}
+
+template<typename T>
+std::optional<MyVector<T>> FindIsotropicVector(MyMatrix<T> const &M, T const &TheMod) {
+  int n = M.rows();
+  if (n == 1) {
+    T val = M(0,0);
+    if (val == 0) {
+      MyVector<T> V(1);
+      V(0) = 1;
+      return V;
+    }
+    return {};
+  }
+  if (n == 2) {
+    return FindIsotropicVectorTwoDim(M, TheMod);
+  }
+  return FindIsotropicVectorRandom(M, TheMod);
+}
+
+
+
+
+
+template<typename T>
+struct ResultNullspaceMod {
+  int dimNSP;
+  MyMatrix<T> BasisTot;
+};
+
 
 /*
   We work with the matrix M with m rows and n cols.
@@ -209,10 +275,16 @@ MyVector<T> FindIsotropicVector(MyMatrix<T> const &M, T const &TheMod) {
   This gets us
   (x, y) / M     \
          \ p I_n /
-  And so we can find the 
+  And so we can find a basis of the kernel.
+
+  What we want to have is a Z-basis (x^1, ...., x^d, .... x^m)
+  with (x^1, ...., x^d) image in (F_p)^m is spanning the
+  kernel over Fp of M.
+
+
  */
 template<typename T>
-MyMatrix<T> NullspaceMatMod(MyMatrix<T> const& M, T const& TheMod) {
+ResultNullspaceMod<T> NullspaceMatMod(MyMatrix<T> const& M, T const& TheMod) {
   int n_row = M.rows();
   int n_col = M.cols();
   MyMatrix<T> Mbig(n_row + n_col, n_col);
@@ -253,18 +325,7 @@ MyMatrix<T> NullspaceMatMod(MyMatrix<T> const& M, T const& TheMod) {
     std::cerr << "This is not what we expected\n";
     throw TerminalException{1};
   }
-#endif
-  // 3) Compute the orthogonal space and then 
-  MyMatrix<T> Morth = NullspaceMat(NSPred);
-  MyMatrix<T> NSPret = NullspaceIntMat(Morth);
-#ifdef DEBUG_MATRIX_MOD
-  dim rnkB = RankMat(NSPred);
-  if (rnkB != dimNSP) {
-    std::cerr << "We have rnkB=" << rnkB << " dimNSP=" << dimNSP << "\n";
-    std::cerr << "This is not what we expected\n";
-    throw TerminalException{1};
-  }
-  MyMatrix<T> prod = NSPret * M;
+  MyMatrix<T> prod = NSPred * M;
   for (int i=0; i<prod.rows(); i++) {
     for (int j=0; j<prod.cols(); j++) {
       T res = ResInt(prod(i,j), TheMod);
@@ -275,10 +336,32 @@ MyMatrix<T> NullspaceMatMod(MyMatrix<T> const& M, T const& TheMod) {
     }
   }
 #endif
-  return NSPret;
+  MyMatrix<T> BasisComp = SubspaceCompletionInt(NSPred, n_row);
+  MyMatrix<T> FullMat = Concatenate(NSPred, BasisComp);
+  return {dimNSP, std::move(FullMat)};
 }
 
-
+template<typename T>
+int DimensionKernelMod(MyMatrix<T> const& M, T const& TheMod) {
+  int n_row = M.rows();
+  int n_col = M.cols();
+  MyMatrix<T> Mbig(n_row + n_col, n_col);
+  for (int i=0; i<n_row; i++) {
+    for (int j=0; j<n_col; j++) {
+      Mbig(i, j) = M(i, j);
+    }
+  }
+  for (int i=0; i<n_col; i++) {
+    for (int j=0; j<n_col; j++) {
+      T val(0);
+      if (i == j)
+        val = TheMod;
+      Mbig(i + n_row, j) = val;
+    }
+  }
+  MyMatrix<T> NSP = NullspaceIntMat(Mbig);
+  return NSP.rows();
+}
 
 // clang-format off
 #endif  // SRC_MATRIX_MAT_MATRIXMOD_H_
