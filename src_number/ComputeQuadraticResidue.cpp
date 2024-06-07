a41e4a7e38a3ab9c8330d5887a8d604400dbc6bc
Mathieu Dutour Sikiric <mathieu.dutour@gmail.com>
2024-06-07 19:58:40 +0200

    Add the program for computing the quadratic residues.

---
+++ src_number/ComputeQuadraticResidue.cpp
@@ -0,0 +1,58 @@
+// Copyright (C) 2022 Mathieu Dutour Sikiric <mathieu.dutour@gmail.com>
+// clang-format off
+#include "NumberTheory.h"
+#include "NumberTheoryBoostCppInt.h"
+#include "NumberTheoryBoostGmpInt.h"
+#include "NumberTheorySafeInt.h"
+#include "TypeConversion.h"
+#include "quadratic_residue.h"
+// clang-format on
+
+template <typename T>
+void process(std::string const& a_str, std::string const& p_str, std::string const& OutFormat, std::ostream& os) {
+  T a = ParseScalar<T>(a_str);
+  T p = ParseScalar<T>(p_str);
+
+  bool test = is_quadratic_residue(a, p);
+  if (OutFormat == "GAP") {
+    os << "return " << test << ";\n";
+    return;
+  }
+  std::cerr << "Failed to find a matching entry for OutFormat\n";
+  throw TerminalException{1};
+}
+
+int main(int argc, char *argv[]) {
+  try {
+    if (argc != 3 && argc != 5) {
+      std::cerr << "Program is used as\n";
+      std::cerr << "ComputeQuadraticResidue [a] [p] [OutFormat] [FileOut]\n";
+      std::cerr << "or\n";
+      std::cerr << "ComputeQuadraticResidue [a] [p]\n";
+      return 1;
+    }
+    std::string a_str = argv[1];
+    std::string p_str = argv[2];
+    std::string OutFormat = "GAP";
+    std::string FileOut = "stderr";
+    using T = mpz_class;
+    if (argc == 5) {
+      std::string OutFormat = argv[3];
+      std::string FileOut = argv[4];
+    }
+    if (FileOut == "stderr") {
+      process<T>(a_str, p_str, OutFormat, std::cerr);
+    } else {
+      if (FileOut == "stdout") {
+        process<T>(a_str, p_str, OutFormat, std::cout);
+      } else {
+        std::ofstream os(FileOut);
+        process<T>(a_str, p_str, OutFormat, os);
+      }
+    }
+    std::cerr << "Normal termination of the program\n";
+  } catch (TerminalException const &e) {
+    std::cerr << "There is a bug to be resolved in the code\n";
+    exit(e.eVal);
+  }
+}
--- src_number/quadratic_residue.h
+++ src_number/quadratic_residue.h
@@ -13,6 +13,51 @@
 #define DEBUG_QUADRATIC_RESIDUE
 #endif
 
+
+
+
+/*
+  References used below:
+  OB: Oswald Baumgart, "The Quadratic Reciprocity Law: A collection of classical proofs"
+     Edited and translated by Franz Lemmermeyer
+
+  Th (OB[End of Chapter 2]): If P and Q are positive coprime integers
+      then we have
+      (P / Q)  (Q / P) = (-1)^( (P-1)/2 . (Q-1)/2 )
+
+  Could we use the theorem to compute the residue in practice?
+  * If the numbers are not coprime.
+    We want to test the existence of a solution for
+    x^2 = P + alpha Q
+    But P and Q have a common factor say h.
+    That reduces to
+    If H has a common square, then we reduce on both sides and all is well.
+    BUT: If there are primes like h that occur only one time.
+    Then x has to to be divisible by h. That reduces the equation to
+    h x^2 = P + alpha Q     with P and Q not divisible by h
+    What to do next?
+    h is invertible in Z/QZ since it is not a factor.
+    Take the invert of h and the equation reduces to
+    x^2 = h^{-1} P mod Q
+    So, we have indeed a reduction of complexity.
+    The number h^{-1} P is coprime with Q since if it were not we would
+    not have been able to invert.
+  * Now, if 2 occurs as a prime factor of both, we can apply the same
+    trick above.
+  * We have the fomula
+    (2 / P) = (-1)^{ (P^2 - 1)/8 }  See Suppelementary Laws in OB
+  * We have (p / 2) = 1 for the odd case.
+
+  But maybe the best is to compute with Kronecker algorithm
+  Problem is conditions exposed in 
+
+  
+ */
+
+
+
+
+
 // This is an exhaustive search that works even if m is not prime.
 template <typename T>
 std::optional<T> find_quadratic_residue(T const &a, T const &m_in) {
